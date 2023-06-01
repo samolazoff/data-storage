91918bc815262c2dbd73c4648b9f5b4f0c87bb7b
unknown <samolazov.roman.alex@gmail.com>
2023-06-01 16:44:56 +0300

    CLIENT: setting redux

---
+++ client/src/reducers/fileReduser.js
@@ -0,0 +1,14 @@
+const defaultState = {
+
+};
+
+const fileReducer = (state = defaultState, action) => {
+
+    switch (action.type){
+        default: 
+            return state
+    }
+
+};
+
+export default fileReducer;
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+    currentUser: {},
+    isAuth: false
+};
+
+const userReducer = (state = defaultState, action) => {
+
+    switch (action.type){
+        default: 
+            return state
+    }
+
+};
+
+export default userReducer;
